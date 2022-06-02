5aef95e536b740fecfb2d7401fea5bef9a4fd145
Johannes Borchardt <borchardt@andlabs.com>
2022-06-02 16:25:37 +0900

    Add basic parsing of excel sheets

--- client/src/components/Upload.vue
+++ client/src/components/Upload.vue
@@ -11,7 +11,7 @@
                 <v-file-input accept="application/vnd.ms-excel, application/msexcel, application/x-msexcel, application/x-ms-excel, 
     application/x-excel, application/x-dos_ms_excel, application/xls, application/x-xls,
     application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" label="Click here to select a file to upload"
-                  outlined v-model="chosenFile" show-size placeholder="Pick a file to upload">
+                  outlined @change="uploadFile" show-size placeholder="Pick a file to upload">
                 </v-file-input>
               </v-card-text>
               <v-card-actions>
@@ -34,16 +34,33 @@
   </div>
 </template>
 <script lang="ts">
+import { read, readFile, utils, writeXLSX } from "xlsx";
+
 export default {
   data() {
     return {
-      chosenFile: '',
-      data: null
+      data: ''
     }
   },
   methods: {
-    uploadFile() {
-      console.log('upload' + this.chosenFile)
+    uploadFile(event) { /* currently not actually uploading, just parsing */
+      var chosenFile = event.target.files[0];
+
+      console.log('upload' + chosenFile)
+
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        /* Parse data */
+        const bstr = e.target.result
+        const wb = read(bstr, { type: 'binary' })
+        /* Get first worksheet */
+        const wsname = wb.SheetNames[0]
+        const ws = wb.Sheets[wsname]
+
+        console.log(ws)
+      }
+      reader.readAsBinaryString(chosenFile);
     }
   }
 }
