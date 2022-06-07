1880bca9565f5a21ebb56510f2d56de3b397d3a2
Draško Kokić <dk@kodra.net>
2022-06-07 21:20:02 +0200

    [server] patch

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,16 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PhoenixModule } from './phoenix/phoenix.module';
 
 @Module({
   imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
     PhoenixModule,
   ],
   controllers: [AppController],
