68df774b09fe2f91ea2ff3adfc9afe8ec177b6b1
Draško Kokić <dk@kodra.net>
2022-06-07 21:20:46 +0200

    [server] Add GraphQL modifications

--- server/src/phoenix/benefit-matrices/benefit-matrices.module.ts
+++ server/src/phoenix/benefit-matrices/benefit-matrices.module.ts
@@ -4,10 +4,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { BenefitMatrix, BenefitMatrixSchema } from './schemas/benefit-matrix.schema';
 import { BanefitMatricesController } from './benefit-matrices.controller';
 import { BanefitMatricesService } from './benefit-matrices.service';
+import { BenefitMatrixResolver } from './benefit-matrix.resolver';
 
 @Module({
     imports: [MongooseModule.forFeature([{ name: BenefitMatrix.name, schema: BenefitMatrixSchema }], 'PhoenixDbConnection')],
     controllers: [BanefitMatricesController],
-    providers: [BanefitMatricesService],
+    providers: [BanefitMatricesService, BenefitMatrixResolver],
 })
 export class BenefitMatricesModule {}
--- server/src/phoenix/benefit-matrices/schemas/benefit-matrix.schema.ts
+++ server/src/phoenix/benefit-matrices/schemas/benefit-matrix.schema.ts
@@ -1,16 +1,21 @@
+import { Directive, Field, ID, ObjectType } from '@nestjs/graphql';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
 export type BenefitMatrixDocument = BenefitMatrix & Document;
 
+@ObjectType()
 @Schema({ collection: 'benefit-matrices' })
 export class BenefitMatrix {
+  @Field(type => ID)
   @Prop()
   name: string;
 
+  @Field()
   @Prop()
   age: number;
 
+  @Field()
   @Prop()
   breed: string;
 }
