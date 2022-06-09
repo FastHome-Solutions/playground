4f58ae6148007eb5932893ff0630c3d79f33ef37
Johannes Borchardt <borchardt@andlabs.com>
2022-06-09 14:03:18 +0900

    [client] Add Apollo

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import { apolloProvider } from './services/BenefitMatrixService'
 
 loadFonts()
 
 createApp(App)
   .use(router)
   .use(vuetify)
+  .use(apolloProvider)
   .mount('#app')
