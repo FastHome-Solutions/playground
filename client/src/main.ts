a89063fec52a30bea82544bd5ee1332abcff252d
Draško Kokić <dk@kodra.net>
2022-06-16 12:54:02 +0200

    [client] patch

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
-import { apolloProvider } from './services/BenefitMatrixService'
+import { apolloProvider } from './graphql'
 import { createPinia } from 'pinia'
 
 loadFonts()
