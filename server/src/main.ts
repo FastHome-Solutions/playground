9f7e3b9f2d4a07fdd8cd174fe5d5ae0041f7d602
Johannes Borchardt <borchardt@andlabs.com>
2022-07-08 17:55:15 +0900

    [server] Increase payload limit

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { json } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.use(json({ limit: '50mb' }));
   await app.listen(process.env.SERVER_PORT ? parseInt(process.env.SERVER_PORT, 10) : 8080);
 }
 bootstrap();
