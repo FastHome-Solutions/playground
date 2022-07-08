14a7614085886c672ea4345e51afbd51fadca941
Johannes Borchardt <borchardt@andlabs.com>
2022-07-08 17:54:55 +0900

    [client] Add multiple contract support to parsing

--- client/src/utils/parsing.utils.ts
+++ client/src/utils/parsing.utils.ts
@@ -63,22 +63,41 @@ export function parseMetadataSuggestions(filename: String, spreadsheet: [][]): S
 
     const metadata = new SpreadsheetMetadata(new Date(from), new Date(till), 'D76', 'N106', 'Online')
     return metadata
-
 }
 
 export function parseSpreadsheet(metadata: SpreadsheetMetadata, spreadsheet: [[]]): BenefitMatrixDto {
-    const headerRowNumber = metadata.rangeStart.replace(/[^0-9]/gi, '') - 1
+    const headerRowNumber = removeChars(metadata.rangeStart) - 1
     const lastContentRowNumber = metadata.rangeEnd.replace(/[^0-9]/gi, '') - 1
     const firstContentColumn = excelColumnToIndex(metadata.rangeStart) - 1
     const lastContentColumn = excelColumnToIndex(metadata.rangeEnd) - 1
-    const firstBundlePriceColumn = 12 - 1 // L
-    const numberOfTariffs = lastContentColumn - firstBundlePriceColumn
+    let firstBundlePriceColumn = 12 - 1 // defautl to L
+
+    let firstDiscountIndex = 5
+    let upfrontIndex = 2
+    let tcoIndex = 4
+    let hasMultipleContractDurations = false
 
     const headerRow: [] = spreadsheet[headerRowNumber]
 
+    if (headerRow.includes('Laufzeit')) {
+        // Multiple contract durations
+        hasMultipleContractDurations = true
+        firstDiscountIndex = 6
+        upfrontIndex = 3
+        tcoIndex = 5
+    }
+
+    const firstTariffName = headerRow[firstContentColumn + firstDiscountIndex] // Discount's tariff name
+    for (let i = firstContentColumn + firstDiscountIndex + 1; i <= lastContentColumn; i++) {
+        if (headerRow[i] == firstTariffName) { // First row with the same name as the first discount's, is where bundles start
+            firstBundlePriceColumn = i
+        }
+    }
+    const numberOfTariffs = lastContentColumn - firstBundlePriceColumn
+
     const tariffNames: String[] = []
     for (let i = firstBundlePriceColumn; i <= lastContentColumn; i++) {
-        const tariffName = headerRow[i]
+        const tariffName = removeLineBreaks(headerRow[i]).trim()
         tariffNames.push(tariffName)
     }
 
@@ -89,6 +108,7 @@ export function parseSpreadsheet(metadata: SpreadsheetMetadata, spreadsheet: [[]
 
     const deviceConfigurations: DeviceConfigurationDto[] = []
 
+    var lastDeviceConfig: DeviceConfigurationDto
     for (let i = headerRowNumber + 1; i <= lastContentRowNumber; i++) {
         const row = spreadsheet[i]
         const contractConfigurations: ContractConfigurationDto[] = []
@@ -96,31 +116,39 @@ export function parseSpreadsheet(metadata: SpreadsheetMetadata, spreadsheet: [[]
         for (let i = 0; i <= numberOfTariffs; i++) {
             tariffConfigurations.push(
                 new TariffConfigurationDto(
-                    tariffNames[i], // tarifName
-                    row[firstContentColumn + 5 + i], // discount
-                    'vouchername', // voucherName Todo: For blau, currently equals to discount
-                    row[firstContentColumn + 8 + i], // bundlePrice
+                    tariffNames[i], // tariffName
+                    row[firstContentColumn + firstDiscountIndex + i], // discount
+                    row[firstContentColumn + firstDiscountIndex + i] + '', // voucherName. For blau, currently equals to discount
+                    row[firstBundlePriceColumn + i], // bundlePrice
                 )
-            )    
+            )
         }
 
-        //TODO: Handle multiple contract configurations
+        let contractDuration = 24
+        if (hasMultipleContractDurations) {
+            contractDuration = removeChars(row[firstContentColumn + 2])
+        }
+
         contractConfigurations.push(
             new ContractConfigurationDto(
-                24, //contractDuration
-                row[firstContentColumn + 2], // upfront
+                contractDuration,
+                row[firstContentColumn + upfrontIndex], // upfront
                 tariffConfigurations,
             )
         )
 
-        deviceConfigurations.push(
-            new DeviceConfigurationDto(
+        if (lastDeviceConfig && lastDeviceConfig.manufacturer == row[firstContentColumn] && lastDeviceConfig.deviceName == row[firstContentColumn + 1]) {
+            lastDeviceConfig.contractConfigurations = lastDeviceConfig.contractConfigurations.concat(contractConfigurations)
+        } else {
+            const deviceConfiguration = new DeviceConfigurationDto(
                 row[firstContentColumn], // manufacturer
                 row[firstContentColumn + 1], // device name
-                row[firstContentColumn + 4], // TCO
+                row[firstContentColumn + tcoIndex], // TCO
                 contractConfigurations,
             )
-        )
+            deviceConfigurations.push(deviceConfiguration)
+            lastDeviceConfig = deviceConfiguration
+        }
     }
 
     const benefitMatrixDto = new BenefitMatrixDto(
@@ -134,7 +162,6 @@ export function parseSpreadsheet(metadata: SpreadsheetMetadata, spreadsheet: [[]
     return benefitMatrixDto
 }
 
-
 function excelColumnToIndex(columnName: String): Number {
     const sanitizedColumnName = columnName.replace(/[0-9]/g, '')
     const numberOfLetters = sanitizedColumnName.length
@@ -146,6 +173,14 @@ function excelColumnToIndex(columnName: String): Number {
         columnIndex += charIndex
     }
     return columnIndex
+}
+
+function removeChars(stringWithChars: String): number {
+    return parseInt(stringWithChars.replace(/[^0-9]/gi, ''))
+}
+
+function removeLineBreaks(stringWithBreaks: String): String {
+    return stringWithBreaks.replace(/(\r\n|\n|\r)/gm, '')
 }
 
 export class SpreadsheetMetadata {
